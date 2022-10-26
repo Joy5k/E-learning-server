ab4f0df70c2c1e89224c2c839687594cd986ebe1
Joy <mmehedihasanjoyv@gmail.com>
2022-10-26 14:44:50 +0600

    creat dynamic link with id

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
+app.get('/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const course = courses.find(cr => cr.id === id);
+    res.send(course);
+})
 
 app.listen(port, () => {
     console.log(`Server is Running on port ${port}`);
